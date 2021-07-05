7d1cc90043a5bd9e94ebb02bdf9c582f8776fc49
Ryan Mendoza <71746816+ryan-mendoza@users.noreply.github.com>
2021-07-04 22:21:06 -0700

    nav complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Nav from './Nav';
 
 function App() {
   return (
     <div className='app'>
       <Header />
-      {/* Navigation */}
+      <Nav />
       {/* Results */}
       {/* Footer */}
     </div>
