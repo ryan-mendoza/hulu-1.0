3d0aa1e9e588d93d08386e2a4d22762401c4fec5
Ryan Mendoza <71746816+ryan-mendoza@users.noreply.github.com>
2021-07-04 22:00:27 -0700

    finished header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hulu Clone</h1>
+    <div className='app'>
+      <Header />
+      {/* Navigation */}
+      {/* Results */}
+      {/* Footer */}
     </div>
   );
 }
